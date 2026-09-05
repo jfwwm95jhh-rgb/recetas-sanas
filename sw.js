3a1fb9618ba418ff98f9e826d6f1bfe7b17841df
jfwwm95jhh-rgb <jfwwm95jhh@privaterelay.appleid.com>
2026-09-05 19:03:10 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,54 +1,59 @@
-const CACHE = "recetas-sanas-v3";
+const CACHE = "recetas-sanas-v4";
+
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json"
+];
 
 self.addEventListener("install", event => {
+  self.skipWaiting();
+
   event.waitUntil(
-    caches.open(CACHE)
-      .then(cache => cache.addAll([
-        "./",
-        "./index.html",
-        "./manifest.json"
-      ]))
-      .then(() => self.skipWaiting())
+    caches.open(CACHE).then(cache => {
+      return cache.addAll(ASSETS);
+    })
   );
 });
 
 self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys()
-      .then(keys => Promise.all(
+    caches.keys().then(keys =>
+      Promise.all(
         keys
           .filter(key => key !== CACHE)
           .map(key => caches.delete(key))
-      ))
-      .then(() => self.clients.claim())
+      )
+    ).then(() => self.clients.claim())
   );
 });
 
 self.addEventListener("fetch", event => {
-  const request = event.request;
 
-  if (
-    request.mode === "navigate" ||
-    request.url.endsWith("/index.html")
-  ) {
+  if (event.request.mode === "navigate") {
+
     event.respondWith(
-      fetch(request)
+      fetch(event.request)
         .then(response => {
+
           const copy = response.clone();
 
-          caches.open(CACHE)
-            .then(cache => cache.put(request, copy));
+          caches.open(CACHE).then(cache => {
+            cache.put("./index.html", copy);
+          });
 
           return response;
         })
-        .catch(() => caches.match(request))
+        .catch(() =>
+          caches.match("./index.html")
+        )
     );
 
     return;
   }
 
   event.respondWith(
-    caches.match(request)
-      .then(cached => cached || fetch(request))
+    caches.match(event.request)
+      .then(cached => cached || fetch(event.request))
   );
 });
